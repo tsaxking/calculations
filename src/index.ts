7f53c53212886365d4c8f78a40d7ed79a4ef750c
Taylor King <taylorking206@u.boisestate.edu>
2025-01-03 10:07:25 -0700

    change package infrastructure

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * as LinearAlgebra from './linear-algebra/index';
+export * as Calculus from './calculus';
+export * as Graphing from './graphing';
+export * as Polygon from './polygon';
+export * as Statistics from './statistics';
---
+++ src/linear-algebra/index.ts
@@ -0,0 +1,6 @@
+export * as MatrixCalcs from './matrix-calculations';
+export * as Matrix from './matrix';
+export * as Plane from './plane';
+export * as Point from './point';
+export * as Spline from './spline';
+export * as Vector from './vector';
